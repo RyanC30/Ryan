7c29da0dde250f0e59a328c4faef9ed7d6fc2d58
RyanC30 <Ryan.P.Crane@students.ittralee.ie>
2017-04-19 17:30:16 +0100

    Initial commit

---
+++ CA/src/test/java/CalculatorIT.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ryanc
+ */
+public class CalculatorIT {
+    
+    public CalculatorIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of add method, of class Calculator.
+     */
+    @org.junit.Test
+    public void testAdd() {
+        System.out.println("add");
+        double a = 0.0;
+        double b = 0.0;
+        Calculator instance = new Calculator();
+        double expResult = 0.0;
+        double result = instance.add(a, b);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of subtract method, of class Calculator.
+     */
+    @org.junit.Test
+    public void testSubtract() {
+        System.out.println("subtract");
+        double a = 0.0;
+        double b = 0.0;
+        Calculator instance = new Calculator();
+        double expResult = 0.0;
+        double result = instance.subtract(a, b);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of multiply method, of class Calculator.
+     */
+    @org.junit.Test
+    public void testMultiply() {
+        System.out.println("multiply");
+        double a = 0.0;
+        double b = 0.0;
+        Calculator instance = new Calculator();
+        double expResult = 0.0;
+        double result = instance.multiply(a, b);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of divide method, of class Calculator.
+     */
+    @org.junit.Test
+    public void testDivide() {
+        System.out.println("divide");
+        double a = 0.0;
+        double b = 0.0;
+        Calculator instance = new Calculator();
+        double expResult = 0.0;
+        double result = instance.divide(a, b);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
