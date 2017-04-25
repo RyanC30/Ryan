6b830aef5b54eefb4de5b3cfc4cfcf1e30d6544b
RyanC30 <Ryan.P.Crane@students.ittralee.ie>
2017-04-25 19:55:26 +0100

    Compiled in Maven to update jar

--- CA/src/main/java/Calculator.java
+++ CA/src/main/java/Calculator.java
@@ -4,7 +4,7 @@
 public class Calculator {
 
     public static void main(String args[]) {
-        System.out.println("Welcome to my calculator");
+        System.out.println("Welcome to my calculator!");
     }
 
         public double add ( double a, double b){
